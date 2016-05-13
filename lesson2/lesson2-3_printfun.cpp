89b87e3199ddb3ce604de2b0c94be1a646dd6f8c
myintelex <myintelex@163.com>
2016-05-13 22:22:19 +0800

    Add the lesson2-3 code

---
+++ lesson2/lesson2-3_printfun.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+void print1()
+{
+	std::cout<<"Three blind mice"<<std::endl;
+}
+
+void print2()
+{
+	std::cout<<"See how they run"<<std::endl;
+}
+
+int main(int argc, char const *argv[])
+{
+	print1();
+	print2();	
+	return 0;
+}
