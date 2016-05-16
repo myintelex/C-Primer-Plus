306f212171c38ac41902c4e244a32b3ecfe667f8
myintelex <myintelex@163.com>
2016-05-16 15:19:02 +0800

    Update lesso3-1_length.cpp

--- lesson3/lesso3-1_length.cpp
+++ lesson3/lesso3-1_length.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
-const int TRANS 12;
+const int TRANS = 12;
 int main()
 {
   int ft_input;
   std::cout<<"Please input your stature"<<std::endl;
   std::cout<<"_"
   std::cin>>ft_input;
-  std::cout<<"Your stature is " << "ft_input / TRANS"<< " and " << "ft_input % TRANS" << std::endl;
+  std::cout<<"Your stature is " << (ft_input / TRANS)<< " ft and " << (ft_input % TRANS)<< " in" << std::endl;
 }
