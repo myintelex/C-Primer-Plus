6a895d9519a26863fcf61b38683b66a2dc71e9b1
myintelex <myintelex@163.com>
2016-05-16 09:50:35 +0800

    Update lesson2-5_cel2fa.cpp

--- lesson2/lesson2-5_cel2fa.cpp
+++ lesson2/lesson2-5_cel2fa.cpp
@@ -4,9 +4,8 @@ int main(int argc, char const *argv[])
 {
 	/* code */
 	int cel;
-	int fa;
 	std::cout << "Please enter a Celsius value:";
 	std::cin>>cel;
-	std::cout << "Your age has " + (cel * 1.8 + 32)<< std::endl;
+	std::cout << "Your age has " << (cel * 1.8 + 32)<< std::endl;
 	return 0;
 }
