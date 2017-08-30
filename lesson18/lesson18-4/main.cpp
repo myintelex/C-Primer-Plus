febffa344c63d89512b46c5305e52118361dfd32
myintelex <myintelex@163.com>
2017-08-30 13:54:57 +0800

    complete the cpp

---
+++ lesson18/lesson18-4/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <algorithm>
+using namespace std;
+
+auto outint = [](int n){ cout << n << " "; };
+
+int main(int argc, char* argv[])
+{
+    int vals[10] = { 50, 100, 90, 180, 60, 210, 415, 88, 188, 201 };
+    list<int> yadayada(vals, vals + 10);
+    list<int> etcetera(vals, vals + 10);
+    cout << "Original lists:\n";
+    for_each(yadayada.begin(), yadayada.end(), outint);
+    cout << endl;
+    for_each(etcetera.begin(), etcetera.end(), outint);
+    cout << endl;
+    yadayada.remove_if([](int v){return v > 100; });
+    etcetera.remove_if([](int v){return v > 200; });
+    cout << "Trimmed lists:\n";
+    for_each(yadayada.begin(), yadayada.end(), outint);
+    cout << endl;
+    for_each(etcetera.begin(), etcetera.end(), outint);
+    cout << endl;
+    return 0;
+}
